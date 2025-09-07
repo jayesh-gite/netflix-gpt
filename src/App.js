fd91a21517fa3c837b878d470b3bcb7001095c17
Jayesh Gite <jayeshgit93@gmail.com>
2025-09-07 23:25:27 +0530

    authentication bug fixes

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
   return (
     <Provider store={appStore}>
       <Body />
-      <div className="text-3xl font-bold text-pink-300 text-fuchsia-300">
-        Namaste Everyone, let's build Netflix GPT
-      </div>
+      <div className="text-3xl font-bold text-pink-300 text-fuchsia-300"></div>
     </Provider>
   );
 }
